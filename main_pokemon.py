2e538e786e07de075e7e182db99664f04c934da8
Satilz <marllon.satil@outlook.com>
2025-01-23 17:19:26 -0300

    Pokemon

---
+++ main_pokemon.py
@@ -0,0 +1,12 @@
+lista = []
+
+lista.append(["Lucario", 34, "masculino", 60])
+lista.append(["Alakazan", 42, "masculino", 60])
+lista.append(["Gengar", 40, "feminino",  67])
+lista.append(["Golem", 44, "masculino", 80])
+lista.append(["Slaking", 40, "feminino", 79])
+lista.append(["Zoroark", 40, "masculino", 60])
+for i in ((lista)):
+    print(f"Nome do pokemon: {i[0]}\nlevel: {i[1]}\ngenero: {i[2]}\nHp: {i[3]}")
+    #print(i)
+
