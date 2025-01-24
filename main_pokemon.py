b9ff84b0ac7ac187199613e11778aa999565093a
Marllon Satil <marllon.satil@outlook.com>
2025-01-24 08:36:30 -0300

    Update main_pokemon.py

--- main_pokemon.py
+++ main_pokemon.py
@@ -7,6 +7,5 @@ lista.append(["Golem", 44, "masculino", 80])
 lista.append(["Slaking", 40, "feminino", 79])
 lista.append(["Zoroark", 40, "masculino", 60])
 for i in ((lista)):
-    print(f"Nome do pokemon: {i[0]}\nlevel: {i[1]}\ngenero: {i[2]}\nHp: {i[3]}")
-    #print(i)
+    print(f"Nome do pokemon: {i[0]}\nlevel: {i[1]}\ngenero: {i[2]}\nHp: {i[3]}\nFoi cadastrado com sucesso!")
 
